505b9efc461fd27d1a5a62487ef9255a7fc1b453
nuraibaimurzaevak <nurai.baimurzaevak@gmail.com>
2026-02-02 13:53:12 +0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,8 +16,12 @@ const orderRoutes = require('./routes/orderRoutes');
 
 
 const app = express();
-const allowedOrigin = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
-
+ const allowedOrigins = [
+      'http://localhost:3000',
+      'http://localhost:5173',
+      'http://127.0.0.1:3000',
+      'http://localhost:10000'
+    ];
 // ⚠️ Настройка CORS: разрешаем доступ с клиента + куки
 app.use(cors({
   origin: allowedOrigin,
